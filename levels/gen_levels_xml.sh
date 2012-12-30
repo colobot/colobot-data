e92dd3d6f8f8af7d41d3eba87cd3c3a6dcdaf6c9
Didier Raboud <odyx@debian.org>
2012-12-30 22:23:43 +0100

    Split po4a configuration in categories
    This has the advantage of dividing the translation in logical blocks
    that are then dispatcheable amongst different translators.

--- levels/gen_levels_xml.sh
+++ levels/gen_levels_xml.sh
@@ -5,12 +5,16 @@ set -e
 lang_short="E F"
 lang_long="en fr"
 
-allsfile_c=levels
+categories="defi free lost perso proto scene1 scene2 scene3 scene4 scene5 scene6 scene7 scene8 scene9 train1 train2 train3 train4 train5 train6 train7"
+empty_categories="win"
+
 allsfile_e=xhtml
 
 gen_i18n_file () {
 
 levelfileorig=$1
+allsfile_c=$2
+
 levelfile=$(echo $levelfileorig | sed -e "s/\.txt$//g")
 
 if [ -z "$levelfileorig" ] || [ ! -f $levelfileorig ]; then
@@ -32,6 +36,7 @@ for lang in $lang_short; do
 	for key in Title Resume ScriptName; do
 		for subkey in text resume; do
 			subval=$(grep "^$key.$lang.*$subkey" $levelfileorig | sed -e "s/^.*$subkey=\"\([^\"]*\)\".*$/\1/")
+			# Always write entries, even when empty, otherwise breaks po4a-gettextize
 			echo "<${key}_$subkey>$levelfile:$subval</${key}_$subkey>" >> $destfile
 			echo "<p type=\"$key $subkey\">$levelfile:$subval</p>" >> $allsfile
 		done
@@ -39,7 +44,7 @@ for lang in $lang_short; do
 	echo "</$levelfile>" >> $destfile
 done
 
-echo "[type:xml] $levelfile.xml \$lang:$levelfile.\$lang.xml" >> po4a.cfg
+echo "[type:xml] $levelfile.xml \$lang:$levelfile.\$lang.xml" >> $allsfile_c-po4a.cfg
 
 echo -n "."
 }
@@ -47,50 +52,56 @@ echo -n "."
 echo "* Cleanup"
 
 rm -f *.xml
-rm -f $allsfile_c.$allsfile_e
-rm -f $allsfile_c.*.$allsfile_e
-rm -f po4a.cfg
+rm -f *po4a.cfg
 
-echo "* Create initial files"
+for category in $categories; do
+echo "* Category: $category "
 
-echo "[po_directory] po/" > po4a.cfg
+	echo " * Create initial files"
 
-echo "<html><body>" > $allsfile_c.$allsfile_e
-for lang in $lang_long; do
-	if [ $lang = "en" ]; then continue; fi;
-	echo "<html><body>" > $allsfile_c.$lang.$allsfile_e
+	echo "[po_directory] $category-po/" > $category-po4a.cfg
+	mkdir -p $category-po
+
+	echo -n " * Generate transitional translation files "
+
+	echo "<html><body>" > $category.$allsfile_e
+	for lang in $lang_long; do
+		if [ $lang = "en" ]; then continue; fi;
+		echo "<html><body>" > $category.$lang.$allsfile_e
+	done
+
+	for level in $(ls $category*.txt); do
+		gen_i18n_file $level $category
+	done
+	echo "done"
+
+	echo "</body></html>" >> $category.$allsfile_e
+
+
+	echo -n " * Generate pristine potfile: "
+	po4a-gettextize -M UTF-8 -f xhtml -m $category.$allsfile_e > $category-po/$category.pot 2>/dev/null
+	echo "done"
+
+	echo -n " * Generate translation files: "
+	for lang in $lang_long; do
+		if [ $lang = "en" ]; then continue; fi;
+		echo -n "$lang"
+		echo "</body></html>" >> $category.$lang.$allsfile_e
+		pofile=$category-po/$lang.po
+		if [ ! -f $pofile ]; then
+			po4a-gettextize -M UTF-8 -L UTF-8 -f xhtml -m $category.$allsfile_e -l $category.$lang.$allsfile_e > $pofile
+			sed -e 's/, fuzzy//g' -i $pofile
+		else
+			po4a-updatepo -M UTF-8 -f xhtml -m $category.$allsfile_e -p $pofile 2>/dev/null
+		fi
+	done
+	echo " done"
+
+	echo -n " * Cleanup po4a infrastructure, run po4a … "
+	po4a -f $category-po4a.cfg 2>/dev/null 1>&2
+	echo "done"
+
 done
-
-echo -n "* Generate intermediate translation files for levels: "
-for level in $(ls *.txt); do
-	gen_i18n_file $level
-done
-echo "done"
-
-echo "</body></html>" >> $allsfile_c.$allsfile_e
-
-echo -n "* Generate pristine potfile: "
-po4a-gettextize -M UTF-8 -f xhtml -m $allsfile_c.$allsfile_e > po/levels.pot 2>/dev/null
-echo "done"
-
-echo -n "* Generate translation files: "
-for lang in $lang_long; do
-	if [ $lang = "en" ]; then continue; fi;
-	echo -n "$lang"
-	echo "</body></html>" >> $allsfile_c.$lang.$allsfile_e
-	pofile=po/$lang.po
-	if [ ! -f $pofile ]; then
-		po4a-gettextize -M UTF-8 -L UTF-8 -f xhtml -m levels.xhtml -l levels.$lang.xhtml > $pofile
-		sed -e 's/, fuzzy//g' -i $pofile
-	else
-		po4a-updatepo -M UTF-8 -f xhtml -m levels.xhtml -p $pofile 2>/dev/null
-	fi
-done
-echo " done"
-
-echo -n "* Cleanup po4a infrastructure, run po4a … "
-po4a -f po4a.cfg 2>/dev/null
-echo "done"
 
 echo -n "* Inject translation in level files: "
 
