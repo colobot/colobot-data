03573d876bb0d4b9aa0fe87fee1efc253e2c61a7
Piotr Dziwinski <piotrdz@gmail.com>
2014-07-09 00:16:59 +0200

    Fix for #310

--- levels-i18n/scripts/03_inject_translations.sh
+++ levels-i18n/scripts/03_inject_translations.sh
@@ -5,12 +5,15 @@ set -e
 levelfile=$1
 translations_prefix=$2
 PODIR=$3
+targetfile=$4
 
 rootfilename=$(basename $translations_prefix)
 
 # Autodetect translated languages
 script_path=$(dirname $0)
 linguas=$(cd $PODIR/; ls *.po | sed -e 's/\.po$//g')
+
+echo -n "" > $targetfile
 
 # Make sure we take english (first, but it's not really important)
 for lang in en $linguas; do
@@ -20,9 +23,9 @@ for lang in en $linguas; do
 	fi
 	i18nfile=$translations_prefix$dotlang.txt
 	if [ -f $i18nfile ]; then
-		sed -n '/^Title/p;/^Resume/p;/^ScriptName/p' $i18nfile
+		sed -n '/^Title/p;/^Resume/p;/^ScriptName/p' $i18nfile >> $targetfile
 	fi
 done
-echo "// End of level headers translations"
-echo ""
-sed -e '/^Title/d;/^Resume/d;/^ScriptName/d' $levelfile
+echo "// End of level headers translations" >> $targetfile
+echo "" >> $targetfile
+sed -e '/^Title/d;/^Resume/d;/^ScriptName/d' $levelfile >> $targetfile
