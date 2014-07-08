b3b82147de8a2c05477752650564180b374c039d
Piotr Dziwinski <piotrdz@gmail.com>
2014-07-09 00:20:15 +0200

    Some refactoring and whitespace fixes

--- levels-i18n/scripts/03_inject_translations.sh
+++ levels-i18n/scripts/03_inject_translations.sh
@@ -17,14 +17,14 @@ echo -n "" > $targetfile
 
 # Make sure we take english (first, but it's not really important)
 for lang in en $linguas; do
-	dotlang=".$lang"
-	if [ "$lang" = "en" ]; then
-		dotlang="";
-	fi
-	i18nfile=$translations_prefix$dotlang.txt
-	if [ -f $i18nfile ]; then
-		sed -n '/^Title/p;/^Resume/p;/^ScriptName/p' $i18nfile >> $targetfile
-	fi
+    dotlang=".$lang"
+    if [ "$lang" = "en" ]; then
+        dotlang="";
+    fi
+    i18nfile=$translations_prefix$dotlang.txt
+    if [ -f $i18nfile ]; then
+        sed -n '/^Title/p;/^Resume/p;/^ScriptName/p' $i18nfile >> $targetfile
+    fi
 done
 echo "// End of level headers translations" >> $targetfile
 echo "" >> $targetfile
